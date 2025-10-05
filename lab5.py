b5786f06eb62006d446b7f030a27b3710b45d322
ezrabuenkanove <ezra.buenk@anove.agency>
2025-10-05 20:41:43 +0200

    feat: Add echo waves and network size determination

--- lab5.py
+++ lab5.py
@@ -32,6 +32,15 @@ class Neighbour:
     last_seen: float
 
 
+@dataclass
+class Wave:
+    children_waiting: set[tuple[int, int]]
+    operation: int = sensor.OP_NOOP
+    parent: tuple[int, int] | None = None
+    payload_sum: int = 0
+    target: tuple[int, int] | None = None
+
+
 # Get random position in NxN grid.
 def random_position(n):
     x = randint(0, n)
@@ -48,68 +57,41 @@ def calculate_distance(point_a, point_b):
 
 
 class SensorNode:
-    def __init__(self, mcast_addr, position, strength, value, ping_period):
+    def __init__(
+        self, mcast_addr, position, strength, value, ping_period, grid_size
+    ):
+        self.listener = MulticastListener(mcast_addr)
+        self.peer_messenger = PeerMessenger()
+
         self.mcast_addr = mcast_addr
         self.position = position
         self.strength = strength
         self.value = value
         self.ping_period = ping_period
+        self.grid_size = grid_size
 
         self.neighbours: dict[tuple[int, int], Neighbour] = {}
-        self.next_ping_at = (
-            time.time() + ping_period if ping_period > 0 else None
-        )
-
-        # Will be defined in start
-        self.mcast = None
-        self.peer = None
+        self.next_ping_at = time.time()
         self.window = None
 
     def start(self):
-        # Create the multicast listener socket.
-        self.mcast = socket.socket(
-            socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP
-        )
+        self.peer_messenger.start()
+        self.listener.start()
 
-        # Sets the socket address as reusable so you can run multiple instances
-        # of the program on the same machine at the same time.
-        self.mcast.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-        # Subscribe the socket to multicast messages from the given address.
-        mreq = struct.pack(
-            "4sl", socket.inet_aton(self.mcast_addr[0]), socket.INADDR_ANY
-        )
-        self.mcast.setsockopt(
-            socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq
-        )
-        if sys.platform == "win32":  # windows special case
-            self.mcast.bind(("localhost", self.mcast_addr[1]))
-        else:  # should work for everything else
-            self.mcast.bind(self.mcast_addr)
-
-        # Create the peer-to-peer socket.
-        self.peer = socket.socket(
-            socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP
-        )
-
-        # Set the socket multicast TTL so it can send multicast messages.
-        self.peer.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 5)
-
-        # Bind the socket to a random port.
-        if sys.platform == "win32":  # windows special case
-            self.peer.bind(("localhost", socket.INADDR_ANY))
-        else:  # should work for everything else
-            self.peer.bind(("", socket.INADDR_ANY))
-
-        # Get the IP and port of the socket.
-        ip, port = self.peer.getsockname()
+        ip, port = self.peer_messenger.get_address()
         self.ip = ip
         self.port = port
 
         # make the gui.
         self.window = MainWindow()
-        self.window.writeln("my address is %s:%s" % self.peer.getsockname())
+        self.window.writeln(
+            "my address is %s:%s" % self.peer_messenger.get_address()
+        )
         self.window.writeln("my position is (%s, %s)" % self.position)
+
+        self.wave_controller = EchoWaveController(
+            self, self.peer_messenger, self.window.writeln
+        )
 
         # This is the event loop.
         try:
@@ -124,78 +106,72 @@ class SensorNode:
             pass
 
     def _handle_incoming_messages(self):
+        sockets = [self.listener.socket, self.peer_messenger.socket]
+
         # Read any incoming messages
-        rlist, _, _ = select.select([self.mcast, self.peer], [], [], 0.05)
-        for sock in rlist:
-            data, address = sock.recvfrom(4096)
+        rlist, _, _ = select.select(sockets, [], [], 0.05)
 
-            try:
-                decoded_message = sensor.message_decode(data)
-            except Exception:
-                print("Error: Received was not in the proper format.")
-                continue
+        if self.listener.socket in rlist:
+            message, address = self.listener.poll()
+            message_type = message[0]
 
-            message_type = decoded_message[0]
+            if message_type == sensor.MSG_PING:
+                self._handle_ping(message, address)
 
-            if message_type == sensor.MSG_PING and sock is self.mcast:
-                self._handle_ping(decoded_message, address)
-            elif message_type == sensor.MSG_PONG and sock is self.peer:
-                self._handle_pong(decoded_message, address)
-            else:
-                pass
+        if self.peer_messenger.socket in rlist:
+            message, address = self.peer_messenger.poll()
+            message_type = message[0]
+
+            if message_type == sensor.MSG_PONG:
+                self._handle_pong(message, address)
+            elif message_type == sensor.MSG_ECHO:
+                self.wave_controller.handle_echo(message, address)
+            elif message_type == sensor.MSG_ECHO_REPLY:
+                self.wave_controller.handle_echo_reply(message, address)
 
     def _periodic_ping(self):
         now = time.time()
         if self.ping_period > 0 and now >= self.next_ping_at:
-            self.send_ping(self.mcast_addr)
+            self.peer_messenger.send_ping(
+                self.mcast_addr, self.position, self.position, self.strength
+            )
             self.next_ping_at = now + self.ping_period
 
-    def send_pong(self, address, initiator_position):
-        msg = sensor.message_encode(
-            sensor.MSG_PONG,
-            0,
-            initiator_position,
-            self.position,
-            (0, 0),
-            0,
-            self.strength,
-            0,
-        )
-
-        self.peer.sendto(msg, address)
-
-    def send_ping(self, address):
-        msg = sensor.message_encode(
-            sensor.MSG_PING,
-            0,
-            self.position,
-            self.position,
-            (0, 0),
-            0,
-            self.strength,
-            0,
-        )
-
-        self.peer.sendto(msg, address)
+        # remove old/stale neighbours
+        ttl = 3 * self.ping_period
+        for pos in list(self.neighbours.keys()):
+            if now - self.neighbours[pos].last_seen > ttl:
+                del self.neighbours[pos]
 
     def _handle_pong(self, decoded_message, address):
         neighbour_position = decoded_message[3]
+        if neighbour_position == self.position:
+            return
+
+        neighbour_strength = decoded_message[6]
+
         distance = calculate_distance(self.position, neighbour_position)
-        if distance <= self.strength:
+        if distance <= neighbour_strength:
             self.neighbours[neighbour_position] = Neighbour(
                 ip=address[0],
                 port=address[1],
-                strength=self.strength,
+                strength=neighbour_strength,
                 distance=distance,
                 last_seen=time.time(),
             )
 
     def _handle_ping(self, decoded_message, address):
         initiator_position = decoded_message[2]
+        if initiator_position == self.position:
+            return
+
         initiator_strength = decoded_message[6]
+
         distance = calculate_distance(self.position, initiator_position)
         if distance <= initiator_strength:
-            self.send_pong(address, initiator_position)
+            self.peer_messenger.send_pong(
+                address, initiator_position, self.position, self.strength
+            )
 
     def _handle_gui_commands(self):
         line = self.window.getline()
@@ -206,13 +182,349 @@ class SensorNode:
 
         if cmd == "properties":
             self.window.writeln(
-                f"{self.position};{self.value};{self.strength};{self.ip};{self.port}"
+                f"{self.position};{self.value};{self.strength};{self.ip}:{self.port}"
             )
         elif cmd == "ping":
-            self.send_ping(self.mcast_addr)
+            self.peer_messenger.send_ping(
+                self.mcast_addr, self.position, self.position, self.strength
+            )
         elif cmd == "list":
             for location, neighbour in self.neighbours.items():
                 self.window.writeln(f"{location};{neighbour.distance}")
+                # TODO: Order this by nearest
+        elif cmd == "move":
+            x = int(parts[1])
+            y = int(parts[2])
+            if len(parts) != 3:
+                self.window.writeln("usage: move <x> <y>")
+            elif x > self.grid_size or y > self.grid_size:
+                self.window.writeln("x and y must be within grid")
+            else:
+                self.position = (x, y)
+                self._periodic_ping()  # Re-ping to adjust neighbours
+        elif cmd == "strength":
+            if len(parts) != 2:
+                self.window.writeln("usage: strength <new_value>")
+            elif int(parts[1]) < 0:
+                self.window.writeln("strength must be greater than 0")
+            else:
+                self.strength = int(parts[1])
+        elif cmd == "echo":
+            self.wave_controller.start_echo_wave()
+        elif cmd == "size":
+            self.wave_controller.start_echo_wave(sensor.OP_SIZE)
+
+
+class MulticastListener:
+    def __init__(self, mcast_addr):
+        self.mcast_addr = mcast_addr
+        self._sock = None
+
+    def start(self):
+        # Create the multicast listener socket.
+        self._sock = socket.socket(
+            socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP
+        )
+
+        # Sets the socket address as reusable so you can run multiple instances
+        # of the program on the same machine at the same time.
+        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        # Subscribe the socket to multicast messages from the given address.
+        mreq = struct.pack(
+            "4sl", socket.inet_aton(self.mcast_addr[0]), socket.INADDR_ANY
+        )
+        self._sock.setsockopt(
+            socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq
+        )
+        if sys.platform == "win32":  # windows special case
+            self._sock.bind(("localhost", self.mcast_addr[1]))
+        else:  # should work for everything else
+            self._sock.bind(self.mcast_addr)
+
+    def poll(self):
+        data, address = self._sock.recvfrom(4096)
+
+        try:
+            decoded_message = sensor.message_decode(data)
+        except Exception:
+            self.window.writeln(
+                "Error: Received message was not in the proper format."
+            )
+            return
+
+        return decoded_message, address
+
+    @property
+    def socket(self):
+        return self._sock
+
+
+class PeerMessenger:
+    def __init__(self):
+        self._sock = None
+
+    def start(self):
+        # Create the peer-to-peer socket.
+        self._sock = socket.socket(
+            socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP
+        )
+
+        # Set the socket multicast TTL so it can send multicast messages.
+        self._sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 5)
+
+        # Bind the socket to a random port.
+        if sys.platform == "win32":  # windows special case
+            self._sock.bind(("localhost", socket.INADDR_ANY))
+        else:  # should work for everything else
+            self._sock.bind(("", socket.INADDR_ANY))
+
+    def poll(self):
+        data, address = self._sock.recvfrom(4096)
+
+        try:
+            decoded_message = sensor.message_decode(data)
+        except Exception:
+            self.window.writeln(
+                "Error: Received message was not in the proper format."
+            )
+            return
+
+        return decoded_message, address
+
+    def get_address(self):
+        ip, port = self._sock.getsockname()
+        return ip, port
+
+    def send_pong(
+        self, address, initiator_position, sender_position, strength
+    ):
+        msg = sensor.message_encode(
+            sensor.MSG_PONG,
+            0,
+            initiator_position,
+            sender_position,
+            (0, 0),
+            0,
+            strength,
+            0,
+        )
+
+        self._sock.sendto(msg, address)
+
+    def send_ping(
+        self, address, initiator_position, sender_position, strength
+    ):
+        msg = sensor.message_encode(
+            sensor.MSG_PING,
+            0,
+            initiator_position,
+            sender_position,
+            (0, 0),
+            0,
+            strength,
+            0,
+        )
+
+        self._sock.sendto(msg, address)
+
+    def send_echo(
+        self,
+        address,
+        initiator_position,
+        sequence_number,
+        sender_position,
+        strength,
+        operation=sensor.OP_NOOP,
+        payload=0,
+    ):
+        msg = sensor.message_encode(
+            sensor.MSG_ECHO,
+            sequence_number,
+            initiator_position,
+            sender_position,
+            (0, 0),
+            operation,
+            strength,
+            payload,
+        )
+
+        self._sock.sendto(msg, address)
+
+    def send_echo_reply(
+        self,
+        address,
+        initiator_position,
+        sequence_number,
+        sender_position,
+        strength,
+        operation=sensor.OP_NOOP,
+        payload=0,
+    ):
+        msg = sensor.message_encode(
+            sensor.MSG_ECHO_REPLY,
+            sequence_number,
+            initiator_position,
+            sender_position,
+            (0, 0),
+            operation,
+            strength,
+            payload,
+        )
+
+        self._sock.sendto(msg, address)
+
+    @property
+    def socket(self):
+        return self._sock
+
+
+class EchoWaveController:
+    def __init__(self, node: SensorNode, messenger: PeerMessenger, log):
+        self.node = node
+        self.msg = messenger
+        self.log = log
+        self.waves_sent = 0
+        self.ongoing_waves: dict[tuple[tuple[int, int], int], Wave] = {}
+
+    def start_echo_wave(self, operation=sensor.OP_NOOP):
+        children = set(self.node.neighbours.keys())
+        origin = self.node.position
+        print(f"{origin} - Initiating echo wave.")
+
+        self.ongoing_waves[(origin, self.waves_sent)] = Wave(
+            parent=None, children_waiting=children, operation=operation
+        )
+
+        if not children:
+            if operation == sensor.OP_SIZE:
+                self.log("size=1")
+            else:
+                self.log(f"The wave {self.waves_sent} has decided.")
+
+            del self.ongoing_waves[(origin, self.waves_sent)]
+
+        for neighbour in self.node.neighbours.values():
+            self.msg.send_echo(
+                (neighbour.ip, neighbour.port),
+                origin,
+                self.waves_sent,
+                origin,
+                self.node.strength,
+                operation,
+            )
+
+        self.waves_sent += 1
+
+    def handle_echo(self, decoded_message, address):
+        initiator_position = decoded_message[2]
+        sequence_number = decoded_message[1]
+        sender_position = decoded_message[3]
+        operation = decoded_message[5]
+
+        origin = self.node.position
+
+        # Check if we've already seen this wave.
+        if (initiator_position, sequence_number) not in self.ongoing_waves:
+            print(f"{origin} - Not seen this wave.")
+            children = set(self.node.neighbours.keys()) - {sender_position}
+
+            # Add wave to state.
+            self.ongoing_waves[(initiator_position, sequence_number)] = Wave(
+                parent=sender_position,
+                children_waiting=children,
+                operation=operation,
+            )
+            print(f"{origin} - Added wave to state.")
+
+            # No children (leaf node), ECHO_REPLY immediately.
+            if not children:
+                print(f"{origin} - No children.")
+                parent_address = (
+                    self.node.neighbours[sender_position].ip,
+                    self.node.neighbours[sender_position].port,
+                )
+
+                self.msg.send_echo_reply(
+                    parent_address,
+                    initiator_position,
+                    sequence_number,
+                    origin,
+                    self.node.strength,
+                    operation,
+                    1 if operation == sensor.OP_SIZE else 0,
+                )
+
+                return
+
+            # Forward ECHO message to children only.
+            for child_position in children:
+                print(f"{origin} - Sent echo messages to children.")
+                child = self.node.neighbours[child_position]
+                self.msg.send_echo(
+                    (child.ip, child.port),
+                    initiator_position,
+                    sequence_number,
+                    origin,
+                    self.node.strength,
+                    operation,
+                )
+
+            return
+
+        # Already participating in wave, send ECHO_REPLY.
+        self.msg.send_echo_reply(
+            address,
+            initiator_position,
+            sequence_number,
+            origin,
+            self.node.strength,
+            operation,
+        )
+        print(f"{origin} - Already participating in wave, sent echo reply.")
+
+    def handle_echo_reply(self, decoded_message, address):
+        initiator_position = decoded_message[2]
+        sequence_number = decoded_message[1]
+        sender_position = decoded_message[3]
+        operation = decoded_message[5]
+        payload = decoded_message[7]
+
+        wave = self.ongoing_waves[(initiator_position, sequence_number)]
+        wave.children_waiting.discard(sender_position)
+
+        wave.payload_sum += payload if operation == sensor.OP_SIZE else 0
+        upstream_payload = wave.payload_sum + 1
+
+        # Check if children waiting set is empty
+        if not wave.children_waiting:
+            if wave.parent is None:
+                if operation == sensor.OP_SIZE:
+                    self.log(f"size={upstream_payload}")
+                else:
+                    self.log(f"The wave {sequence_number} has decided.")
+            else:
+                self.log(
+                    f"{(sequence_number, initiator_position)}: Received from all neighbours."
+                )
+
+                parent_position = wave.parent
+                parent_address = (
+                    self.node.neighbours[parent_position].ip,
+                    self.node.neighbours[parent_position].port,
+                )
+
+                self.msg.send_echo_reply(
+                    parent_address,
+                    initiator_position,
+                    sequence_number,
+                    self.node.position,
+                    self.node.strength,
+                    operation,
+                    upstream_payload if operation == sensor.OP_SIZE else 0,
+                )
+
+            del self.ongoing_waves[(initiator_position, sequence_number)]
 
 
 # Additional parameters to this function must always have a default value.
@@ -234,7 +546,12 @@ def main(
     """
 
     new_sensor = SensorNode(
-        mcast_addr, sensor_pos, sensor_strength, sensor_value, ping_period
+        mcast_addr,
+        sensor_pos,
+        sensor_strength,
+        sensor_value,
+        ping_period,
+        grid_size,
     )
 
     new_sensor.start()
--- sensor.py
+++ sensor.py
@@ -2,8 +2,8 @@
 Networks and Network Security
 Lab 5 - Distributed Sensor Network
 
-NAME:
-STUDENT ID:
+NAME: Ezra Buenk
+STUDENT ID: 15817814
 
 DESCRIPTION: Definitions and message format
 """
@@ -21,7 +21,9 @@ MSG_ECHO_REPLY = 3  # Unicast echo reply.
 OP_NOOP = 0  # Do nothing.
 OP_SIZE = 1  # Compute the size of network.
 OP_UPDATE = 2  # Force update the network.
-# TODO: You may define your own echo operations if needed.
+OP_ROUTE_HOPS = 3
+OP_ROUTE_STRENGTH = 4
+OP_ROUTE_FETCH = 5
 
 # This is used to pack message fields into a binary format.
 message_format = struct.Struct("!iiiiiiiiiif")
@@ -68,5 +70,13 @@ def message_decode(buffer):
     type, sequence, ix, iy, nx, ny, tx, ty, operation, strength, payload = (
         message_format.unpack(buffer)
     )
-    return (type, sequence, (ix, iy), (nx, ny), (tx, ty), operation, strength,
-            payload)
+    return (
+        type,
+        sequence,
+        (ix, iy),
+        (nx, ny),
+        (tx, ty),
+        operation,
+        strength,
+        payload,
+    )
